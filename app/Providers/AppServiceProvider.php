beebe91c91d92240ad24c605a65afa800f10e4e1
Gideon Zz <gilangrakar@gmail.com>
2026-04-23 17:27:14 +0700

    appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production') || env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
+
         Inertia::share('flash', fn () => [
             'success' => Session::get('success'),
             'error' => Session::get('error'),
